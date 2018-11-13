dcb57ff2b7d60efd70641af09687ece9ac495dca
cp7 <chenpengcp7@163.com>
2018-11-13 15:57:00 -0600

    using map solve the problem of valid anagram
    the important thing is check if the corresponding value of one key is null, and subtract 1 from one value after checking

---
+++ src/Set/Lc242.java
@@ -0,0 +1,29 @@
+package Set;
+
+/**
+ * Cr
+ * eated by CP on 13/11/2018.
+ */
+
+import java.util.*;
+
+public class Lc242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        Map<Character, Integer> sMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (sMap.get(s.charAt(i)) == null) sMap.put(s.charAt(i), 1);
+            else sMap.put(s.charAt(i), sMap.get(s.charAt(i)) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (sMap.get(t.charAt(i)) == null)
+                return false;
+            else {
+                sMap.put(t.charAt(i), sMap.get(t.charAt(i)) - 1);
+                if (sMap.get(t.charAt(i)) == 0)
+                    sMap.remove(t.charAt(i));
+            }
+        }
+        return true;
+    }
+}
