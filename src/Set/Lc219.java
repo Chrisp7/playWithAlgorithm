7d767d04e548da2eb8ee8618223a18aad9ecac58
cp7 <chenpengcp7@163.com>
2018-11-18 23:19:31 -0600

    complete Lc219
    Because we already konw the window size is k, and it's predetermined, so we can focus on keeping the size of set to make it equal to k

---
+++ src/Set/Lc219.java
@@ -0,0 +1,18 @@
+package Set;
+
+import java.util.*;
+
+/**
+ * Created by CP on 18/11/2018.
+ */
+public class Lc219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) return true;
+            else set.add(nums[i]);
+            if (set.size() == k + 1) set.remove(nums[i - k]);
+        }
+        return false;
+    }
+}
