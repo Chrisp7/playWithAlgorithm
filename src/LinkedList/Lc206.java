65e26ac2fdb68e65f010ffb50fe0f88f57c4a25c
cp7 <chenpengcp7@163.com>
2018-11-20 01:05:05 -0600

    complete Lc206 Reverse linked list
    use three pointers to do the reverse, and pay attention on the order of changing pointers. Also, you have to make sure all the pointer are pointing to a valid area, which involves checking if the head is null

---
+++ src/LinkedList/Lc206.java
@@ -0,0 +1,21 @@
+package LinkedList;
+
+/**
+ * Created by CP on 20/11/2018.
+ */
+
+
+public class Lc206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+}
---
+++ src/LinkedList/ListNode.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+/**
+ * Created by CP on 20/11/2018.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
