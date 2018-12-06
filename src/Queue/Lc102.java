3518b6c0d5fb2a8978a9a1a6d61f38a77d967b33
cp7 <chenpengcp7@163.com>
2018-12-05 20:39:14 -0600

    complete Lc102
    note that the queue is often used to do breath-first traversal. Also, for here, because we have to record the level of the node we are visiting, so we have to create a class to reocrd the relation between node and it's level.

---
+++ src/Queue/Lc102.java
@@ -0,0 +1,40 @@
+package Queue;
+
+import java.util.*;
+
+/**
+ * Created by CP on 2018/12/5.
+ */
+public class Lc102 {
+    public class NodeWithLevel {
+        TreeNode node;
+        int level;
+
+        public NodeWithLevel(TreeNode node, int level) {
+            this.node = node;
+            this.level = level;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<NodeWithLevel> queue = new LinkedList<>();
+        queue.add(new NodeWithLevel(root, 0));
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        while (!queue.isEmpty()) {
+            NodeWithLevel cur = queue.remove();
+            if (cur.node.left != null) queue.add(new NodeWithLevel(cur.node.left, cur.level + 1));
+            if (cur.node.right != null) queue.add(new NodeWithLevel(cur.node.right, cur.level + 1));
+
+            if (cur.level == res.size()) {
+                res.add(new ArrayList<Integer>());
+                res.get(cur.level).add(cur.node.val);
+            } else {
+                res.get(cur.level).add(cur.node.val);
+            }
+
+        }
+        return res;
+    }
+}
---
+++ src/Queue/TreeNode.java
@@ -0,0 +1,14 @@
+package Queue;
+
+/**
+ * Created by CP on 2018/12/5.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
