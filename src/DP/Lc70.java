8fe5fd2b7505d098a32461134c891c6a736840b8
cp7 <chenpengcp7@163.com>
2019-01-18 23:34:51 -0600

    Complete Lc70
    using memory search to avoid duplicate search

---
+++ src/DP/Lc70.java
@@ -0,0 +1,18 @@
+package DP;
+
+public class Lc70 {
+    private int[] arr;
+
+    private int calcWays(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (arr[n - 1] != 0) return arr[n - 1];
+        else arr[n - 1] = calcWays(n - 1) + calcWays(n - 2);
+        return arr[n - 1];
+    }
+
+    public int climbStairs(int n) {
+        arr = new int[n];
+        return calcWays(n);
+    }
+}
