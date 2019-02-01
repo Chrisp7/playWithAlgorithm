1de6b21c22d821c7f13c8d6ad9b3d96edd67caea
cp7 <chenpengcp7@163.com>
2019-01-31 23:08:52 -0600

    Complete Lc253
    think about yourself as a time manager, so you should stand at the point of start time, how to allocate each start time a room, if it's within other meeting's end time, then you should allocate another room, if not you can directly use the room just available.

---
+++ src/liked/Lc253.java
@@ -0,0 +1,38 @@
+package liked;
+import java.util.*;
+
+public class Lc253 {
+    class Interval {
+        int start;
+        int end;
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
+
+    public int minMeetingRooms(Interval[] intervals) {
+        int len = intervals.length;
+        int[] starts = new int[len];
+        int[] ends = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            starts[i] = intervals[i].start;
+            ends[i] = intervals[i].end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int res = 0;
+        int end = 0;
+        for (int i = 0; i < starts.length; i++) {
+            if (starts[i] < ends[end]) res++;
+            else end++;
+        }
+        return res;
+    }
+}
