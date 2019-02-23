f293dcbd6e7c352deb8339a063bbfbfe0b0b9111
cp7 <chenpengcp7@163.com>
2019-02-23 01:18:59 -0600

    Complete Lc56
    much like meeting room, also use the idea of scanning all the start to see if it intersect with end

---
+++ src/liked/Lc56.java
@@ -0,0 +1,40 @@
+package liked;
+
+import java.util.*;
+
+public class Lc56 {
+    public class Interval {
+        int start;
+        int end;
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
+
+    public List<Interval> merge(List<Interval> intervals) {
+        if (intervals == null || intervals.size() <= 1) return intervals;
+        Collections.sort(intervals, (a, b) -> (a.start - b.start));
+        List<Interval> res = new ArrayList<>();
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
+
+        for (Interval interval : intervals) {
+            if (interval.start <= end) {
+                end = Math.max(end, interval.end);
+            } else {
+                res.add(new Interval(start, end));
+                start = interval.start;
+                end = interval.end;
+            }
+        }
+        res.add(new Interval(start, end));
+        return res;
+    }
+}
