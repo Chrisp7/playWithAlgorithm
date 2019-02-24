b5b2c7abe1c17201a92fd73bcb829d486e5e1319
cp7 <chenpengcp7@163.com>
2019-02-24 15:40:10 -0600

    Complete Lc53
    use the idea of dp to solve the problem. Focus on the array instead of the sum.

---
+++ src/liked/Lc53.java
@@ -0,0 +1,15 @@
+package liked;
+
+public class Lc53 {
+    public int maxSubArray(int[] nums) {
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = nums[i] + (dp[i - 1] < 0 ? 0 : dp[i - 1]);
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
