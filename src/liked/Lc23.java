af5ad0ed6c093eabf12902e8e5d2fc9b59e4469d
cp7 <chenpengcp7@163.com>
2019-02-24 20:07:55 -0600

    add another method to solve Lc23
    use the idea of divide and conquer to solve the problem

--- src/liked/Lc23.java
+++ src/liked/Lc23.java
@@ -31,4 +31,31 @@ public class Lc23 {
         }
         return Dummy.next;
     }
+
+    public ListNode mergeKLists2(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        return sort(lists, 0, lists.length - 1);
+    }
+
+    private ListNode sort(ListNode[] lists, int low, int high) {
+        if (low >= high) return lists[low];
+        int mid = (high - low) / 2 + low;
+        ListNode left = sort(lists, low, mid);
+        ListNode right = sort(lists, mid + 1, high);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        if (left == null) return right;
+        if (right == null) return left;
+        if (left.val < right.val) {
+            left.next = merge(left.next, right);
+            return left;
+        } else {
+            right.next = merge(left, right.next);
+            return right;
+        }
+    }
+
 }
